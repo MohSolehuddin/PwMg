13aa522a123815e92d022abc8cd7baf8f05afdd2
MohSolehuddin <moh.solehuddin190805@gmail.com>
2024-04-30 23:49:28 +0700

    fix: automation intallation fuetures

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 echo "setup instalation.."
-mkdir private
-node setup.js
-
-sleep .3
-rm -r setup.js
-rm -r install.sh
-rm -r README.md
+mkdir -p private
+file="./private/pw.json"
+if [ ! -f "$file" ]; then
+    echo '{"passwords":[]}' > "$file"
+fi

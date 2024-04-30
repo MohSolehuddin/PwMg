09017fc3ca2a8fb5c3c20b48ddfbfa0360d75d30
MohSolehuddin <moh.solehuddin190805@gmail.com>
2024-05-01 00:17:02 +0700

    fix: instalation features

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
-echo "setup instalation.."
+echo "perangkat yanag anda gunakan?"
+echo "1. Android"
+echo "2. Windows"
+echo "3. Linux"
+echo "4. MacOS"
+read device
+
+pkg install nodejs-lts -y
+echo "install nodejs-lts sukses!!!"
+sleep .3
+npm isntall
 mkdir -p private
 file="./private/pw.json"
 if [ ! -f "$file" ]; then
     echo '{"passwords":[]}' > "$file"
 fi
+echo "setup success!!!..."
--- setup.js
+++
@@ -1,2 +0,0 @@
-const fs = require('fs');
-fs.writeFile("./private/pw.json", `{"passwords":[]}`);

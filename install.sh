a090da229b83e0d5b598d57c606244d7512ce791
MohSolehuddin <moh.solehuddin190805@gmail.com>
2024-04-30 12:43:39 +0700

    add: instalation features

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo "setup instalation.."
+mkdir private
+node setup.js
+
+sleep .3
+rm -r setup.js
+rm -r install.sh
+rm -r README.md
---
+++ setup.js
@@ -0,0 +1,2 @@
+const fs = require('fs');
+fs.writeFile("./private/pw.json", `{"passwords":[]}`);
